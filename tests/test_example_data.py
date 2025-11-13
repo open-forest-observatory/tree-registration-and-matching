27aad32b9c0d8ce887ca8105167794a0394fea89
David Russell <djrussell@ucdavis.edu>
2025-11-13 20:09:24 +0000

    Add test for example data

---
+++ tests/test_example_data.py
@@ -0,0 +1,6 @@
+from tree_registration_and_matching.example_data import simulate_tree_maps
+
+
+def test_example_data():
+    """Test the example data generation function."""
+    simulate_tree_maps()
