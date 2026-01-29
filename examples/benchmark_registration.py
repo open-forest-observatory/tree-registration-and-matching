31be1ce96059e7439a2472c60fb84fe36fe321fd
David Russell <djrussell@ucdavis.edu>
2026-01-29 16:46:28 +0000

    Fix issues and add trendline

--- examples/benchmark_registration.py
+++ examples/benchmark_registration.py
@@ -37,11 +37,21 @@ if RUN_MEE:
     )
 
 if RUN_CHM:
+
+    def find_best_shift_specialized(tree_points, CHM, plot_bounds):
+        return find_best_shift(
+            tree_points=tree_points,
+            CHM=CHM,
+            plot_bounds=plot_bounds,
+            x_range=(-12, 12, 0.25),
+            y_range=(-12, 12, 0.25),
+        )
+
     shifts_CHM = score_approach(
         field_trees_folder=FIELD_TREES,
         CHMs_or_detected_trees_folder=CHMS,
         plots_file=PLOTS_FILE,
-        alignment_algorithm=find_best_shift,
+        alignment_algorithm=find_best_shift_specialized,
         CHM_approach=True,
         crop_to_plot_bounds=True,
         plot_buffer_distance=10,
@@ -56,16 +66,29 @@ if VIS:
     CHM_shifts = np.load(OUTPUT_CHM)
     MEE_shifts = np.load(OUTPUT_MEE)
 
+    # Replace nans with the worst value
+    CHM_shifts = np.nan_to_num(CHM_shifts, nan=12)
+
     # Show the magtides of the error for indivdual plots with the two approaches
     CHM_errors = np.linalg.norm(CHM_shifts, axis=1)
     MEE_errors = np.linalg.norm(MEE_shifts, axis=1)
 
     plt.title("MEE error magnitudes vs. CHM error magnitudes")
     plt.scatter(MEE_errors, CHM_errors)
+
+    # Compute trend line between MEE and CHM errors
+    z = np.polyfit(MEE_errors, CHM_errors, 1)
+    p = np.poly1d(z)
+    x_trend = np.linspace(MEE_errors.min(), MEE_errors.max(), 100)
+    print(x_trend)
+    print(p(x_trend))
+    plt.plot(x_trend, p(x_trend), "r--", label=f"Trend line: y={z[0]:.2f}x+{z[1]:.2f}")
+
     plt.xlabel("MEE error magnitudes")
     plt.ylabel("CHM error magnitudes")
-    plt.xlim(0, np.sqrt(2) * 12)
-    plt.ylim(0, np.sqrt(2) * 12)
+    plt.xlim(-0.5, np.sqrt(2) * 12.5)
+    plt.ylim(-0.5, np.sqrt(2) * 12.5)
+    plt.legend()
     plt.show()
 
     # Show the x, y coordinates of the errors for the two approaches on separate subplots
@@ -73,10 +96,10 @@ if VIS:
     ax[0].scatter(MEE_shifts[:, 0], MEE_shifts[:, 1])
     ax[1].scatter(CHM_shifts[:, 0], CHM_shifts[:, 1])
 
-    ax[0].set_xlim((-12, 12))
-    ax[0].set_ylim((-12, 12))
-    ax[1].set_xlim((-12, 12))
-    ax[1].set_ylim((-12, 12))
+    ax[0].set_xlim((-12.5, 12.5))
+    ax[0].set_ylim((-12.5, 12.5))
+    ax[1].set_xlim((-12.5, 12.5))
+    ax[1].set_ylim((-12.5, 12.5))
 
     ax[0].set_title("MEE displacements")
     ax[1].set_title("CHM displacements")
